243422bae511eca92c490140522b67d2045b811a
Troy Pavlek <troy@tpavlek.me>
2015-07-31 14:15:45 -0600

    Added a new interface to represent values we expect to get back as entity Ids

---
+++ src/Domain/IdValueInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Depotwarehouse\Blumba\Domain;
+
+/**
+ * Interface IdValueInterface
+ *
+ * This contract is implemented by any Id Value in the system. Id Values are Value Objects that encapsulate the Ids
+ * for any particular entity.
+ *
+ * @package Depotwarehouse\Blumba\Domain
+ */
+interface IdValueInterface extends ValueObjectInterface
+{
+
+}
