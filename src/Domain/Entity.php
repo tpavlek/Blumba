23a52653e9e784288b333bf08369dff8aabe60a7
Troy Pavlek <troy@tpavlek.me>
2015-08-14 11:33:58 -0600

    Added basic abstract Entity

---
+++ src/Domain/Entity.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Depotwarehouse\Blumba\Domain;
+
+abstract class Entity implements EntityInterface
+{
+    /**
+     * Compute equality of the identity of this entity with another entity.
+     *
+     * @param EntityInterface $otherEntity
+     * @return boolean
+     */
+    public function equals(EntityInterface $otherEntity)
+    {
+        return ($otherEntity instanceof static) && $this->getId()->toString() === $otherEntity->getId()->toString();
+    }
+
+}
